aeab14caa2e5d8f3200fffe3c4851cf411199a30
yonog1 <yonatangoldin69@gmail.com>
2022-07-03 21:17:40 +0300

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
 import logo from "./favicon.ico";
 import "./App.css";
-import { Navbar, Button, Alert, Breadcrumb, Card } from "react-bootstrap";
+import {
+    Navbar,
+    Button,
+    Alert,
+    Breadcrumb,
+    Card,
+    Nav,
+    NavDropdown,
+} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
     return (
         <div className="App">
-            <Navbar bg="Nav-Styling" variant="light" sticky="fix">
+            <Navbar bg="Nav-Styling" variant="light" sticky="fix" expand="sm">
                 <Navbar.Brand>
                     <img className="Nav-Image" src={logo}></img>
                     Musicle
                 </Navbar.Brand>
-                <Navbar.Toggle>
-                    <Navbar.Collapse>y=</Navbar.Collapse>
-                </Navbar.Toggle>
+                <Nav>
+                    <Nav.Link href="user-stats">Stats</Nav.Link>
+                    <Nav.Link href="info">More Info</Nav.Link>
+                    <NavDropdown>
+                        <Nav.Link href="how-to-play">How to play</Nav.Link>
+                        <Nav.Link href="about">About</Nav.Link>
+                        <Nav.Link href="contact">Contact Us</Nav.Link>
+                    </NavDropdown>
+                </Nav>
             </Navbar>
             <div className="content">This is content.</div>
             <header className="App-header">
