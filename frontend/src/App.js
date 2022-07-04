5714483ef30dc5d50e8b477473e605c5084278e2
yonog1 <yonatangoldin69@gmail.com>
2022-07-04 19:53:23 +0300

    small ui changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,14 +10,6 @@ function App() {
         <div className="App">
             <Navegation />
             <header className="App-header">
-                <Card className="Card-Image" style={{ color: "#000" }}>
-                    <Card.Img src="./favicon.ico" width="50" />
-                    <Card.Body></Card.Body>
-                    <Card.Title>example</Card.Title>
-                    <Card.Text>example</Card.Text>
-                    <Button variant="success">Read more</Button>
-                </Card>
-
                 <Button variant="success" size="lg">
                     Test button
                 </Button>
--- frontend/src/Navegation.js
+++ frontend/src/Navegation.js
@@ -6,19 +6,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Navegation() {
     return (
-        <Navbar bg="Nav-Styling" variant="light" sticky="fix" expand="sm">
-            <Navbar.Brand>
-                <img className="Nav-Image" src={logo}></img>
-                Musicle
-            </Navbar.Brand>
-            <Nav>
-                <Nav.Link href="user-stats">Stats</Nav.Link>
-                <NavDropdown title="More Info">
-                    <Nav.Link href="how-to-play">How to play</Nav.Link>
-                    <Nav.Link href="about">About</Nav.Link>
-                    <Nav.Link href="contact">Contact Us</Nav.Link>
-                </NavDropdown>
-            </Nav>
-        </Navbar>
+        <div>
+            <Navbar
+                bg="Nav-Styling"
+                variant="light"
+                sticky="fix"
+                expand="sm"
+                className="navbar"
+            >
+                <Navbar.Brand>
+                    <img className="Nav-Image" src={logo}></img>
+                    <a href="/" className="homepage-link">
+                        Musicle
+                    </a>
+                </Navbar.Brand>
+                <Nav>
+                    <Nav.Link href="user-stats">Stats</Nav.Link>
+                    <NavDropdown title="More Info">
+                        <Nav.Link href="how-to-play">How to play</Nav.Link>
+                        <Nav.Link href="about">About</Nav.Link>
+                        <Nav.Link href="contact">Contact Us</Nav.Link>
+                    </NavDropdown>
+                </Nav>
+            </Navbar>
+        </div>
     );
 }
