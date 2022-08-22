d9baf904051d62b6c84a25b1dc406cbc2cf668a0
yonog1 <yonatangoldin69@gmail.com>
2022-08-22 18:23:46 +0300

    added server file, re-wrote auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navegation from "./Navegation";
 import SearchBar from "./SearchBar";
 import LoginButton from "./LoginButton";
-import SpotifyPlayer from 'react-spotify-web-playback';
+import SpotifyPlayer from "react-spotify-web-playback";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 function App() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => setData(data.message));
+    }, []);
+
     return (
         <div className="App" style={{ height: "100%" }}>
             <Navegation />
             <div className="App-header">
+                <p>{!data ? "Loading..." : data}</p>
                 <LoginButton />
                 <SpotifyPlayer />
                 <SearchBar />
--- frontend/src/LoginButton.js
+++ frontend/src/LoginButton.js
@@ -6,15 +6,19 @@ import { Button, Container } from "react-bootstrap";
 import React from "react";
 import env from "react-dotenv";
 
-const clientId = "ad5ca9a8802d424db83c8c16085dcbae"; //here ill use some env variables
-const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET //here ill use some env variables
-console.log(process.env.REACT_APP_SPOTIFY_CLIENT_SECRET, 'fff');
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID; //here ill use some env variables
+const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET; //here ill use some env variables
+
 const AUTH_URL = ``;
 export default function LoginButton() {
     return (
         <Container>
-            <p>{process.env.REACT_APP_SPOTIFY_CLIENT_ID}</p>
-            <Button href={AUTH_URL} variant="success" size="lg" style={{marginBottom:'15px'}}>
+            <Button
+                href={AUTH_URL}
+                variant="success"
+                size="lg"
+                style={{ marginBottom: "15px" }}
+            >
                 Log In with Spotify
             </Button>
         </Container>
